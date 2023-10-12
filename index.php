c209b6cc4fb37bca004386dcf260788f277a6362
Vũ Mạnh Chiến <111623502+manhchien16@users.noreply.github.com>
2023-10-12 12:35:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/index.css">
-    <title>Document</title>
+    <title>Trang chủ</title>
 </head>
 
 
